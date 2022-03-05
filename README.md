29122da3417f5f08d4b1cf72fdca888fe88b2b7f
KyungIn Lee <jess.lee2906@gmail.com>
2022-03-05 16:53:47 +1100

    Added javascript to show checkbox

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# 03-password-generator
+:::::::::::::::::::::::::::::::::::::::
+::::::: # 03-password-generator :::::::
+:::::::::::::::::::::::::::::::::::::::
+
+## Project Description
+To create and deploy a
+
+
+## Acceptance Criteria
+
+At a minimum,
+
+## Challenges
+
+I attempted 
+
+## Next steps
+
+
+## Resources
+- For base structure
+https://www.section.io/engineering-education/how-to-build-a-random-password-generator-app-with-html-css-javascript/
+
+- Checkbox events
+https://html.form.guide/html-form/html-checkbox-events/
---
+++ script.js
@@ -0,0 +1,108 @@
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// UX preference: Would not include this additional step however homework specification
+    window.onload=function(){
+        var r=confirm("Please specify password criteria");
+        }
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// Getting the DOM Elements
+const resultDOM = document.getElementById("result");
+const copybtnDOM = document.getElementById("copy");
+const lengthDOM = document.getElementById("length");
+const uppercaseDOM = document.getElementById("uppercase");
+const numbersDOM = document.getElementById("numbers");
+const symbolsDOM = document.getElementById("symbols");
+const generatebtn = document.getElementById("generate");
+const form = document.getElementById("password-generator");
+
+// Generating Character Codes For The Application
+const UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CODES = arrayFromLowToHigh(97, 122);
+const NUMBER_CODES = arrayFromLowToHigh(48, 57);
+const SYMBOL_CODES = arrayFromLowToHigh(33, 47)
+  .concat(arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHigh(123, 126));
+
+ // Character Code Generating Function
+function arrayFromLowToHigh(low, high) {
+    const array = [];
+    for (let i = low; i <= high; i++) {
+      array.push(i);
+    }
+    return array;
+  } 
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// The Password Generating Function
+//UX preference: Would remove includeLowercase as this would be a default inclusion
+let generatePassword = (
+    characterAmount,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  ) => {
+    //  UX preference: would include the line-> let charCodes = LOWERCASE_CODES; to make lowecase default inclusion and remove the first line below
+    let charCodes = LOWERCASE_CODES;
+    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CODES);
+    if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CODES);
+    if (includeNumbers) charCodes = charCodes.concat(NUMBER_CODES);
+
+    const passwordCharacters = [];
+    for (let i = 0; i < characterAmount; i++) {
+      const characterCode =
+        charCodes[Math.floor(Math.random() * charCodes.length)];
+      passwordCharacters.push(String.fromCharCode(characterCode));
+    }
+    return passwordCharacters.join("");
+}
+
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
+// Copy password button
+ 
+copybtnDOM.addEventListener("click", () => {
+    const textarea = document.createElement("textarea");
+    const passwordToCopy = resultDOM.innerText;
+    // A Case when Password is Empty
+    if (!passwordToCopy) return;
+    // Copy Functionality
+    textarea.value = passwordToCopy;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Password Copied to Clipboard");
+  });
+
+  // Checking the options that are selected and setting the password
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const characterAmount = lengthDOM.value;
+    const includeUppercase = uppercaseDOM.checked;
+    const includeNumbers = numbersDOM.checked;
+    const includeSymbols = symbolsDOM.checked;
+    const password = generatePassword(
+      characterAmount,
+      includeUppercase,
+      includeNumbers,
+      includeSymbols
+    );
+    resultDOM.innerText = password;
+  });
